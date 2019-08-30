6b80a6d465633ef40245eb0a3af09e0310d4348e
KhacDong <khacdongmta98@gmail.com>
2019-08-30 16:22:33 +0700

    commit by dong

--- quanlykhachsan/quanlykhachsan/frmthongtinpphongkh.cs
+++ quanlykhachsan/quanlykhachsan/frmthongtinpphongkh.cs
@@ -11,5 +11,30 @@ using BLL;
 
 namespace quanlykhachsan
 {
-    
+    public partial class frmthongtinpphongkh : Form
+    {
+        public frmthongtinpphongkh()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_load(object sender, EventArgs e)
+        {
+            phongBLL pbll = new phongBLL();
+            dataGridView1.DataSource = pbll.hienthithongtinphong();
+        }
+
+        private void thoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            frmdangnhap frm = new frmdangnhap();
+            frm.Show();
+
+        }
+    }
 }
