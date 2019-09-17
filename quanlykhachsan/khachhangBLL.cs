414e73b7b406c487a3c41ceedae00c10f90ef704
DoVanHao <DoVanHao@users.noreply.github.com>
2019-09-17 09:02:38 -0700

    class khách hàng

---
+++ quanlykhachsan/khachhangBLL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using System.Data;
+
+namespace BLL
+{
+   public class khachhangBLL
+    {
+        khachhangDAL khDAL = new khachhangDAL();
+        public DataTable hienthikhachhang()
+        {
+            return khDAL.hienthikhachhang();
+        }
+        public bool them_khachhang(string tenKhachHang, DateTime ngaySinh, bool gioiTinh, string chungMinhNhanDan, string diaChi, string soDienThoai, string quocTich)
+        {
+            return khDAL.them_khachhang(tenKhachHang, ngaySinh, gioiTinh, chungMinhNhanDan, diaChi, soDienThoai, quocTich);
+        }
+         
+    }
+}
