0fbaafe0dbb2b60eb9cbd9c908d95625175a2e09
Falcon303 <50917954+Falcon303@users.noreply.github.com>
2019-09-19 00:00:08 +0700

    change some code in nhanvienDAL and...

---
+++ quanlykhachsan/DAL/ketnoi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class ketnoi
+    {
+        public static SqlConnection Get()
+        {
+            SqlConnection cnn = new SqlConnection();
+             //@"Data Source=THANH-HOANG-THU;Initial Catalog=quanlykhachsan;Integrated Security=True";
+            cnn.ConnectionString = @"Data Source=DESKTOP-EVMFSFV\SQLEXPRESS;Initial Catalog=quanlykhachsan;Integrated Security=True";
+            cnn.Open();
+            return cnn;
+        }
+   }
+}
