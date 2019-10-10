12a569e13d35c7874804f32a60d5a156ec06010f
MinhHieu <rezo.krifferson@gmail.com>
2019-10-11 00:00:23 +0700

    chinh sua

--- quanlykhachsan/quanlykhachsan/frmkhachhang.cs
+++ quanlykhachsan/quanlykhachsan/frmkhachhang.cs
@@ -67,28 +67,7 @@ namespace quanlykhachsan
             trangthai(true);
 
         }
-        private void hienthi()
-        {
-            khachhangBLL pbll = new khachhangBLL();
-            dataGridView1.DataSource = pbll.hienthikhachhang();
-        }
-        private void trangthai(bool t)
-        {
-           // txtmakhachhang.Enabled = t;
-            txttenkh.Enabled = t;
-            txtcmt.Enabled = t;
-            txtsdt.Enabled = t;
-            txtdiachi.Enabled = t;
-            dtngaysinh.Enabled = t;
-            cbquoctich.Enabled = t;
-            cbgiotinhkh.Enabled = t;
-        }
 
-        private void frm_load(object sender, EventArgs e)
-        {
-            hienthi();
-            trangthai(false);
-        }
         private void btnxoa_Click(object sender, EventArgs e)
         {
             khachhangBLL khBLL = new khachhangBLL();
@@ -137,28 +116,6 @@ namespace quanlykhachsan
                khBLL.xoa_khachhang(int.Parse(txtmakhachhang.Text));
               hienthi();
             }
-        }
-		        private void hienthi()
-        {
-            khachhangBLL pbll = new khachhangBLL();
-            dataGridView1.DataSource = pbll.hienthikhachhang();
-        }
-        private void trangthai(bool t)
-        {
-           // txtmakhachhang.Enabled = t;
-            txttenkh.Enabled = t;
-            txtcmt.Enabled = t;
-            txtsdt.Enabled = t;
-            txtdiachi.Enabled = t;
-            dtngaysinh.Enabled = t;
-            cbquoctich.Enabled = t;
-            cbgiotinhkh.Enabled = t;
-        }
-
-        private void frm_load(object sender, EventArgs e)
-        {
-            hienthi();
-            trangthai(false);
         }
 
         private void btnluu_Click(object sender, EventArgs e)
