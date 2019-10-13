57f24c2172f448e8edb76aa16b4f9df9c6795cfe
Falcon303 <50917954+Falcon303@users.noreply.github.com>
2019-10-13 20:04:57 +0700

    change some code in frm nhanvien

--- quanlykhachsan/quanlykhachsan/frmnhanvien.Designer.cs
+++ quanlykhachsan/quanlykhachsan/frmnhanvien.Designer.cs
@@ -426,6 +426,33 @@
             int i = cmd.ExecuteNonQuery();
             cnn.Close();
         }
+        public void xoa_NV1(int maNhanVien)
+        {
+
+            SqlConnection cnn = ketnoi.Get();
+            SqlCommand cmd = new SqlCommand("xoa_nhanvien", cnn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("maNhanVien", maNhanVien);
+            int i = cmd.ExecuteNonQuery();
+            cnn.Close();
+
+        }
+        public void sua_nhanvien1(string hoTen, bool gioiTinh, DateTime ngaySinh, string soChungMinh, string diaChi, string soDienThoai, DateTime ngayVaoLam, int maNhanVien)
+        {
+            SqlConnection cnn = ketnoi.Get();
+            SqlCommand cmd = new SqlCommand("sua_nhanvien", cnn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("hoTen", hoTen);
+            cmd.Parameters.AddWithValue("gioiTinh", gioiTinh);
+            cmd.Parameters.AddWithValue("ngaySinh", ngaySinh);
+            cmd.Parameters.AddWithValue("soChungMinh", soChungMinh);
+            cmd.Parameters.AddWithValue("diaChi", diaChi);
+            cmd.Parameters.AddWithValue("soDienThoai", soDienThoai);
+            cmd.Parameters.AddWithValue("ngayVaoLam", ngayVaoLam);
+            cmd.Parameters.AddWithValue("maNhanVien", maNhanVien);
+            int i = cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
         #endregion
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.TextBox txtsdt;
